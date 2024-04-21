4f3320e7acd1d71a0276bb3484c6cf94aa7657a9
league <hailinday@gmail.com>
2024-04-21 11:29:55 -0700

    Chuckles

---
+++ src/_01_chuckle_clicker/ChuckleClicker.java
@@ -0,0 +1,16 @@
+package _01_chuckle_clicker;
+
+public class ChuckleClicker {
+public static void main(String[] args) {
+	makeButtons();
+}
+
+private static void makeButtons() {
+	// TODO Auto-generated method stub
+	
+}
+
+}
+
+	
+
