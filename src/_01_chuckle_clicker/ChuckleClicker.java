7b7e934f0da922be3468937955bba5b70e30e05f
league <hailinday@gmail.com>
2024-04-28 11:26:46 -0700

    Jeopardy and jokes

--- src/_01_chuckle_clicker/ChuckleClicker.java
+++ src/_01_chuckle_clicker/ChuckleClicker.java
@@ -1,12 +1,49 @@
 package _01_chuckle_clicker;
 
-public class ChuckleClicker {
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+
+public class ChuckleClicker implements ActionListener {
+	static JButton leftButton = new JButton();
+	static JButton rightButton = new JButton();
+	static JFrame frame = new JFrame();
+	static JPanel panel = new JPanel();
+	void run() {
+		makeButtons();
+	}
 public static void main(String[] args) {
-	makeButtons();
+	ChuckleClicker click = new ChuckleClicker();
+	click.run();
 }
 
-private static void makeButtons() {
+private void makeButtons() {
 	// TODO Auto-generated method stub
+	
+	
+	leftButton.setText("Joke");
+	rightButton.setText("Punchline");
+	leftButton.addActionListener(this);
+	rightButton.addActionListener(this);
+	frame.setVisible(true);
+	frame.add(panel);
+	panel.add(leftButton);
+	panel.add(rightButton);
+	frame.pack();
+}
+
+@Override
+public void actionPerformed(ActionEvent e) {
+	JButton buttonPressed = (JButton) e.getSource();
+	if(e.getSource() == leftButton) {
+		JOptionPane.showMessageDialog(null, "What did the sushi say to the bee?");
+    } else if(e.getSource() == rightButton) {
+    	JOptionPane.showMessageDialog(null, "Wasabi?");
+    }
 	
 }
 
