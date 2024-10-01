ec1625945d7dbcce282d609b52d60ddac9e48e9e
league <league@imac-29.attlocal.net>
2024-09-30 18:29:03 -0700

    Thank f*** this works.

--- src/_10_slot_machine/SlotMachine.java
+++ src/_10_slot_machine/SlotMachine.java
@@ -13,6 +13,7 @@ import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
 public class SlotMachine implements ActionListener {
@@ -32,19 +33,21 @@ public class SlotMachine implements ActionListener {
 	Random ran2 = new Random();
 	Random ran3 = new Random();
 	
-	public SlotMachine(JFrame frame, JPanel panel1, JPanel panel2, JPanel panel3, JPanel panel4, JLabel label1, JLabel label2, JLabel label3, JButton aspectGamble) throws MalformedURLException {
+	public SlotMachine(JFrame frame, JPanel panel1, JPanel panel2, JPanel panel3, JPanel panel4, JButton aspectGamble) throws MalformedURLException {
 		this.frame = frame;
 		this.panel1 = panel1;
 		this.panel2 = panel2;
 		this.panel3 = panel3;
 		this.panel4 = panel4;
-		this.label1 = label1;
+		/*this.label1 = label1;
 		this.label2 = label2;
-		this.label3 = label3;
+		this.label3 = label3;*/
+		this.aspectGamble = aspectGamble;
 		label1 = createLabelImage("Cloak.jpg");
-		label2 = createLabelImage("Blazing.jpg");
+		label2 = createLabelImage("Cloak.jpg");
 		label3 = createLabelImage("Cloak.jpg");
 		this.aspectGamble = aspectGamble;
+		aspectGamble.setText("Reroll");
 		aspectGamble.addActionListener(this);
 		frame.setLayout(new FlowLayout());
 		frame.add(panel1);
@@ -77,7 +80,7 @@ public class SlotMachine implements ActionListener {
 		URL imageURL = getClass().getResource(fileName);
 	if (imageURL == null){
 		System.err.println("Could not find image " + fileName);
-		return new JLabel();
+		return (Icon) new JLabel();
 	}
 	Icon icon = new ImageIcon(imageURL);
 		return icon;
@@ -89,84 +92,137 @@ public static void main(String[] args) throws MalformedURLException {
 	JPanel panel2 = new JPanel();
 	JPanel panel3 = new JPanel();
 	JPanel panel4 = new JPanel();
-	JLabel label1 = new JLabel();
-	JLabel label2 = new JLabel();
-	JLabel label3 = new JLabel();
+//	JLabel label1 = new JLabel();
+//	JLabel label2 = new JLabel();
+//	JLabel label3 = new JLabel();
 	JButton aspectGamble = new JButton();
-	SlotMachine aspect = new SlotMachine(frame, panel1, panel2, panel3, panel4, label1, label2, label3, aspectGamble);
+	SlotMachine aspect = new SlotMachine(frame, panel1, panel2, panel3, panel4, aspectGamble);
+	/*, label1, label2, label3*/
 }
 
 @Override
 public void actionPerformed(ActionEvent e) {
-	number1 = ran1.nextInt(9);
+	JButton temp = (JButton) e.getSource();
+	if(temp.equals(aspectGamble)) {
+	number1 = ran1.nextInt(10);
 	if(number1==0) {
 		label1.setIcon(createIcon("Blazing.jpg"));
 		frame.pack();
 	}else if(number1==1) {
-		label1 = createLabelImage("Blazing.jpg");
+		label1.setIcon(createIcon("Celestine.jpg"));
 		frame.pack();
 	}else if(number1==2) {
-		label1 = createLabelImage("Blazing.jpg");
+		label1.setIcon(createIcon("Gilded.jpg"));
 		frame.pack();
 	}else if(number1==3) {
-		label1 = createLabelImage("Blazing.jpg");
+		label1.setIcon(createIcon("Glacial.jpg"));
 		frame.pack();
 	}else if(number1==4) {
-		label1 = createLabelImage("Blazing.jpg");
+		label1.setIcon(createIcon("Malachite.jpg"));
 		frame.pack();
 	}else if(number1==5) {
-		label1 = createLabelImage("Blazing.jpg");
+		label1.setIcon(createIcon("Mending.jpg"));
 		frame.pack();
 	}else if(number1==6) {
-		label1 = createLabelImage("Blazing.jpg");
+		label1.setIcon(createIcon("Overloading.jpg"));
 		frame.pack();
 	}else if(number1==7) {
-		label1 = createLabelImage("Blazing.jpg");
+		label1.setIcon(createIcon("Perfected.jpg"));
 		frame.pack();
 	}else if(number1==8) {
-		label1 = createLabelImage("Blazing.jpg");
+		label1.setIcon(createIcon("Twisted.jpg"));
+		frame.pack();
+	}else if(number1==9) {
+		label1.setIcon(createIcon("Voidtouched.jpg"));
 		frame.pack();
 	}
-number2 = ran2.nextInt(9);
-if(number1==0) {
-	
-}else if(number1==1) {
-	
-}else if(number1==2) {
-	
-}else if(number1==3) {
-	
-}else if(number1==4) {
-	
-}else if(number1==5) {
-	
-}else if(number1==6) {
-	
-}else if(number1==7) {
-	
-}else if(number1==8) {
-	
+number2 = ran2.nextInt(10);
+if(number2==0) {
+	label2.setIcon(createIcon("Blazing.jpg"));
+	frame.pack();
+}else if(number2==1) {
+	label2.setIcon(createIcon("Celestine.jpg"));
+	frame.pack();
+}else if(number2==2) {
+	label2.setIcon(createIcon("Gilded.jpg"));
+	frame.pack();
+}else if(number2==3) {
+	label2.setIcon(createIcon("Glacial.jpg"));
+	frame.pack();
+}else if(number2==4) {
+	label2.setIcon(createIcon("Malachite.jpg"));
+	frame.pack();
+}else if(number2==5) {
+	label2.setIcon(createIcon("Mending.jpg"));
+	frame.pack();
+}else if(number2==6) {
+	label2.setIcon(createIcon("Overloading.jpg"));
+	frame.pack();
+}else if(number2==7) {
+	label2.setIcon(createIcon("Perfected.jpg"));
+	frame.pack();
+}else if(number2==8) {
+	label2.setIcon(createIcon("Twisted.jpg"));
+	frame.pack();
+}else if(number2==9) {
+	label2.setIcon(createIcon("Voidtouched.jpg"));
+	frame.pack();
 }
-number3 = ran3.nextInt(9);
-if(number1==0) {
-	
-}else if(number1==1) {
-	
-}else if(number1==2) {
-	
-}else if(number1==3) {
-	
-}else if(number1==4) {
-	
-}else if(number1==5) {
-	
-}else if(number1==6) {
-	
-}else if(number1==7) {
-	
-}else if(number1==8) {
-	
+number3 = ran3.nextInt(10);
+if(number3==0) {
+	label3.setIcon(createIcon("Blazing.jpg"));
+	frame.pack();
+}else if(number3==1) {
+	label3.setIcon(createIcon("Celestine.jpg"));
+	frame.pack();
+}else if(number3==2) {
+	label3.setIcon(createIcon("Gilded.jpg"));
+	frame.pack();
+}else if(number3==3) {
+	label3.setIcon(createIcon("Glacial.jpg"));
+	frame.pack();
+}else if(number3==4) {
+	label3.setIcon(createIcon("Malachite.jpg"));
+	frame.pack();
+}else if(number3==5) {
+	label3.setIcon(createIcon("Mending.jpg"));
+	frame.pack();
+}else if(number3==6) {
+	label3.setIcon(createIcon("Overloading.jpg"));
+	frame.pack();
+}else if(number3==7) {
+	label3.setIcon(createIcon("Perfected.jpg"));
+	frame.pack();
+}else if(number3==8) {
+	label3.setIcon(createIcon("Twisted.jpg"));
+	frame.pack();
+}else if(number3==9) {
+	label3.setIcon(createIcon("Voidtouched.jpg"));
+	frame.pack();
 }
+if(number1==0&&number2==0&&number3==0) {
+	JOptionPane.showMessageDialog(null, "Congrats! You obtained Ifrit's Distinction! You are now a Blazing Elite!");
+}else if(number1==1&&number2==1&&number3==1) {
+	JOptionPane.showMessageDialog(null, "Congrats! You obtained Spectral Circlet! You are now a Celestine Elite!");
+}else if(number1==2&&number2==2&&number3==2) {
+	JOptionPane.showMessageDialog(null, "Congrats! You obtained Aurelionite's Blessing! You are now a Gilded Elite!");
+}else if(number1==3&&number2==3&&number3==3) {
+	JOptionPane.showMessageDialog(null, "Congrats! You obtained Her Biting Embrace! You are now a Glacial Elite!");
+}else if(number1==4&&number2==4&&number3==4) {
+	JOptionPane.showMessageDialog(null, "Congrats! You obtained N'kuhana's Retort! You are now a Malachite Elite!");
+}else if(number1==5&&number2==5&&number3==5) {
+	JOptionPane.showMessageDialog(null, "Congrats! You obtained His Reassurance! You are now a Mending Elite!");
+}else if(number1==6&&number2==6&&number3==6) {
+	JOptionPane.showMessageDialog(null, "Congrats! You obtained Silence Between Two Strikes! You are now a Overloading Elite!");
+}else if(number1==7&&number2==7&&number3==7) {
+	JOptionPane.showMessageDialog(null, "Congrats! You obtained Shared Design! You are now a Perfected Elite!");
+}else if(number1==8&&number2==8&&number3==8) {
+	JOptionPane.showMessageDialog(null, "Congrats! You obtained His Spiteful Boon! You are now a Twisted Elite!");
+}else if(number1==9&&number2==9&&number3==9) {
+	JOptionPane.showMessageDialog(null, "A Void Infestor latched on to you! You are now a Voidtouched Elite! I'm not sure that's a good thing.");
+}
+
+	}
 		// TODO Auto-generated catch block
 
 	}
